2255373b1b53b5eaf7d902fbcc884566451550b3
Zverkov Maxim <tlore007@gmail.com>
2024-04-03 23:17:38 +0300

    feat(Field): now cells contains in two dimensional arraylist

--- src/classes/Field.java
+++ src/classes/Field.java
@@ -7,16 +7,18 @@ public class Field {
     private int height;
     private int width;
 
-    private ArrayList<Cell> cells = new ArrayList<Cell>();
+    private ArrayList<ArrayList<Cell>> cells = new ArrayList<ArrayList<Cell>>();
 
     public Field(int height, int width) {
         this.height = height;
         this.width = width;
 
         for (int i = 0; i < height; i++) {
+            ArrayList<Cell> row = new ArrayList<Cell>();
             for (int j = 0; j < width; j++) {
-                cells.add(new Cell(new Point(i, j), this));
+                row.add(new Cell(new Point(i, j), this));
             }
+            cells.add(row);
         }
     }
 
@@ -29,14 +31,14 @@ public class Field {
     }
 
     public Cell getCell(Point coords) {
-        return cells.get(coords.x * width + coords.y);
+        return cells.get(coords.x).get(coords.y);
     }
 
     public String toString() {
         String result = "";
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
-                result += cells.get(i * width + j).toString();
+                result += cells.get(i).get(j).toString();
             }
             result += "\n";
         }
