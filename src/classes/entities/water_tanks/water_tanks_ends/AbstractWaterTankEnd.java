1cf3eca8d4f8ddc5c2d7b7a23b6f5142f5c318d7
Zverkov Maxim <tlore007@gmail.com>
2024-05-31 00:18:34 +0300

    feat(WATERTANKEND): added abstract class for ends

---
+++ src/classes/entities/water_tanks/water_tanks_ends/AbstractWaterTankEnd.java
@@ -0,0 +1,33 @@
+package classes.entities.water_tanks.water_tanks_ends;
+
+import classes.Direction;
+import classes.entities.water_tanks.WaterTank;
+
+public abstract class AbstractWaterTankEnd {
+    Direction _direction;
+    WaterTank _parentWaterTank;
+
+    public AbstractWaterTankEnd(Direction direction) {
+        _direction = direction;
+    }
+
+    public Direction getDirection() {
+        return _direction;
+    }
+
+    public void rotateClockwise() {
+        _direction = _direction.rotateClockwise();
+    }
+
+    public boolean setParentWaterTank(WaterTank parentWaterTank) {
+        _parentWaterTank = parentWaterTank;
+        return true;
+    }
+
+    public abstract WaterTank getConnectedNeighbour();
+
+    public WaterTank getParentWaterTank() {
+        return _parentWaterTank;
+    }
+
+}
